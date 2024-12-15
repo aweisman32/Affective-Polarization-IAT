f94bb88d02f55e4c78d1b69790620fcf2277ae1c
aweisman32 <a_weisman@coloradocollege.edu>
2024-12-14 19:14:09 -0700

    Add files via upload

---
+++ affectiveIAT.js
@@ -0,0 +1,93 @@
+Qualtrics.SurveyEngine.addOnload(function()
+{
+	/*Place your JavaScript here to run when the page loads*/
+
+});
+
+Qualtrics.SurveyEngine.addOnReady(function()
+{
+	/*Place your JavaScript here to run when the page is fully displayed*/
+
+});
+
+Qualtrics.SurveyEngine.addOnUnload(function()
+{
+	/*Place your JavaScript here to run when the page is unloaded*/
+
+});Qualtrics.SurveyEngine.addOnload(function () {
+    // hide question and next button
+    var container = this.getQuestionContainer();
+    container.querySelector('.Inner').style.display = 'none';
+    this.hideNextButton();
+
+    // load MinnoJS from the CDN (you probably don't need to change this)
+    var scriptTag = document.createElement('script');
+    scriptTag.src = 'https://cdn.jsdelivr.net/gh/minnojs/minno-quest@0.3/dist/pi-minno.js';
+    scriptTag.onload = onLoad;
+    scriptTag.onreadystatechange = onLoad;
+    container.appendChild(scriptTag);
+
+    // create the root element for Minno
+    var canvas = document.createElement('div');
+    container.appendChild(canvas);
+
+    // function to proceed to next question
+    var proceed = this.clickNextButton.bind(this);
+
+    // This function gets activated only after MinnoJS is loaded
+    function onLoad() {
+        // Run your study (just set the correct URL)
+        minnoJS(canvas, 'aweisman32.github.io/Affective-Polarization-IAT/affectiveIAT.js');
+
+        // MinnoJS doesn't know about Qualtrics, we pass a function to inject the results into the question
+        // For some reason `piGlobal` isn't available so we attach it to `minnoJS`
+        minnoJS.logger = function (value) {
+            var el = container.querySelector('textarea');
+            el.value = value;
+        }
+
+        // At the end of the study let MinnoJS proceed to the next question
+        // We need to wait a few miliseconds for Qualtrics to register the value that we entered
+        minnoJS.onEnd = function () { setTimeout(proceed, 100); }
+    }
+});define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
+    var API = new APIConstructor();
+
+	return iatExtension({
+		category1 : {
+			name : 'Liberal people', //Will appear in the data.
+			title : {
+				media : {word : 'Liberal people'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {word : 'Progressive'}, 
+    			{word : 'Democrat'}, 
+    			{word : 'Left-wing'}
+    		
+			], 
+			//Stimulus css (style)
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
+		category2 :	{
+			name : 'Conservative people', //Will appear in the data.
+			title : {
+				media : {word : 'Conservative people'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {word : 'Traditional'}, 
+    			{word : 'Republican'}, 
+    			{word : 'Right-wing'}
+   			], 
+			//Stimulus css
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
+
+		base_url : {//Where are your images at?
+			image : 'https://baranan.github.io/minno-tasks/images/'
+		} 
+	});
+});
